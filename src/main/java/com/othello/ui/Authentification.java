d1add6abba2f6b6711f54b78f5cb39267e950676
ferdaousak <akkar88889@gmail.com>
2021-01-01 18:41:36 +0100

    add authentification and a simple home page ( with password cryptage)

---
+++ src/main/java/com/othello/ui/Authentification.java
@@ -0,0 +1,236 @@
+package com.othello.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+import javax.crypto.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rabab
+ */
+public class Authentification extends javax.swing.JFrame {
+
+    Connection connection = null;
+    ResultSet rs = null;
+    ResultSet rs2 = null;
+    PreparedStatement ps = null;
+    EncryptPassword encryptPwd;
+    
+    
+    public Authentification() {
+        initComponents();
+        //--------------------- pour se connecter à la BDD ---------------------------  
+           try
+       { 
+               Class.forName("com.mysql.cj.jdbc.Driver");	 
+               connection = DriverManager.getConnection("jdbc:mysql://localhost/othello_game" ,"root","");
+               System.out.println("Connexion effective !");
+              
+       }
+       catch(ClassNotFoundException | SQLException ex)
+       {
+           System.err.println(ex);
+       } 
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        nomTxt = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        loginBtn = new javax.swing.JButton();
+        mdpTxt = new javax.swing.JPasswordField();
+        resetBtn = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Nom");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel2.setText("Mot de passe");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setText("Authentification");
+
+        loginBtn.setText("S'authentifier");
+        loginBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginBtnActionPerformed(evt);
+            }
+        });
+
+        resetBtn.setText("Réinitialiser");
+        resetBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                resetBtnActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(93, 93, 93)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addGap(70, 70, 70)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(nomTxt)
+                            .addComponent(mdpTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(172, 172, 172)
+                        .addComponent(jLabel3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(184, 184, 184)
+                        .addComponent(loginBtn)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(resetBtn)))
+                .addContainerGap(82, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(jLabel3)
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nomTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(mdpTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginBtn)
+                    .addComponent(resetBtn))
+                .addContainerGap(45, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBtnActionPerformed
+        
+        String userName = this.nomTxt.getText();
+        String password = this.mdpTxt.getText();
+                
+        try {
+            try {
+                encryptPwd = new EncryptPassword();
+            } catch (Exception ex) {
+                Logger.getLogger(Authentification.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            ps = connection.prepareStatement("Select username, password from users where username=? and password=?");
+            ps.setString(1, userName);
+            ps.setString(2, encryptPwd.encrypt(password));
+            rs = ps.executeQuery();
+            
+            if(rs.next()){
+                dispose();  //fermer la page d'authentification
+                Home homeFrame = new Home(userName);
+                homeFrame.setVisible(true);
+                JOptionPane.showMessageDialog(null, "Authentification avec succès!");
+                
+            } else{           
+                String name ;
+                String query2 = "select username from users where username='"+userName+"'";
+                rs2 = ps.executeQuery(query2);
+                if(rs2.next()){
+                    name = rs2.getString("username");
+                    System.out.println(name);
+                    if(name.equals(userName))
+                    JOptionPane.showMessageDialog(null, "Ce nom déjà existe!");
+                    nomTxt.setText("");
+                     mdpTxt.setText("");
+                } else{
+                    String query = "insert into users(username,password) values(? , ?)";
+                    ps = connection.prepareStatement(query);
+                    ps.setString(1, userName);
+                    ps.setString(2, encryptPwd.encrypt(password));
+                    ps.executeUpdate();
+                    dispose();  //fermer la page d'authentification
+                    Home homeFrame = new Home(userName);
+                    homeFrame.setVisible(true);
+                    JOptionPane.showMessageDialog(null, "Authentification avec succès!");
+                }              
+                
+            }
+                        
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }//GEN-LAST:event_loginBtnActionPerformed
+
+    private void resetBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetBtnActionPerformed
+        nomTxt.setText("");
+        mdpTxt.setText("");
+    }//GEN-LAST:event_resetBtnActionPerformed
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Authentification.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Authentification.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Authentification.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Authentification.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Authentification().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JButton loginBtn;
+    private javax.swing.JPasswordField mdpTxt;
+    private javax.swing.JTextField nomTxt;
+    private javax.swing.JButton resetBtn;
+    // End of variables declaration//GEN-END:variables
+}
