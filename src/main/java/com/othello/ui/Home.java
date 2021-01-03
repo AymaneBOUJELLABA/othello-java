168f7724cbacb8fee7eb941a0c51b81ef0b00bfb
ferdaousak <akkar88889@gmail.com>
2021-01-03 13:30:22 +0100

    added home page and choice play ( still need wofk)

--- src/main/java/com/othello/ui/Home.java
+++ src/main/java/com/othello/ui/Home.java
@@ -5,65 +5,171 @@
  */
 package com.othello.ui;
 
+import java.awt.*;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  *
  * @author macbookpro
  */
-public class Home extends javax.swing.JFrame {
+public class Home extends javax.swing.JFrame 
+{
     
-    public Home(){
-        
-    }
-
-    public Home(String userName) {
+    private JPanel panel;
+	public Home(String userName) 
+	{
         initComponents();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() 
+    {
 
-        logoutBtn = new javax.swing.JButton();
+    	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        panel = new JPanel();
+        
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        logoutBtn.setText("Déconnexion");
-        logoutBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                logoutBtnActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        GroupLayout layout = new GroupLayout(getContentPane());
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(304, Short.MAX_VALUE)
-                .addComponent(logoutBtn)
-                .addGap(48, 48, 48))
+        	layout.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(Alignment.LEADING, layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(panel, GroupLayout.DEFAULT_SIZE, 673, Short.MAX_VALUE)
+        			.addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(298, Short.MAX_VALUE)
-                .addComponent(logoutBtn)
-                .addGap(26, 26, 26))
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(panel, GroupLayout.DEFAULT_SIZE, 330, Short.MAX_VALUE)
+        			.addContainerGap())
         );
+        panel.setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("Bienvenue a Othello");
+        lblNewLabel.setFont(new Font("SimSun", Font.BOLD, 16));
+        lblNewLabel.setBounds(235, 42, 201, 19);
+        panel.add(lblNewLabel);
+        
+        
+        
+        JButton newGame = new JButton("Nouveau");
+        newGame.addActionListener(new ActionListener() 
+        {
+        	public void actionPerformed(ActionEvent e) 
+        	{
+        		setContentPane(ChoicePanel());
+        		pack();
+        		
+        	}
+        });
+        newGame.setBounds(258, 110, 137, 23);
+        panel.add(newGame);
+        
+        JButton LoadGame = new JButton("Continue");
+        LoadGame.setBounds(258, 164, 137, 23);
+        panel.add(LoadGame);
+        
+        JButton archives = new JButton("Archives");
+        archives.setBounds(258, 223, 137, 23);
+        panel.add(archives);
+        
+        JButton Deconnexion = new JButton("Déconnexion");
+        Deconnexion.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		int a = JOptionPane.showConfirmDialog(null, "Est ce que vous êtes sur ?");
+                if (a == JOptionPane.YES_OPTION) {
+                          dispose();
+                          Authentification auth = new Authentification();
+                          auth.setVisible(true);
+                      }
+        	}
+        });
+        Deconnexion.setBounds(258, 279, 137, 23);
+        panel.add(Deconnexion);
+        getContentPane().setLayout(layout);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void logoutBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logoutBtnActionPerformed
-        int a = JOptionPane.showConfirmDialog(null, "Est ce que vous êtes sur ?");
-        if (a == JOptionPane.YES_OPTION) {
-            dispose();
-            Authentification auth = new Authentification();
-            auth.setVisible(true);
-        }
-    }//GEN-LAST:event_logoutBtnActionPerformed
+    public JPanel ChoicePanel()
+    {
+    	
+		JPanel Gamepanel = new JPanel();
+		Gamepanel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Joueur vs IA");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(60, 135, 122, 23);
+		Gamepanel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Joueur vs Joueur");
+		btnNewButton_1.setBounds(316, 135, 135, 23);
+		Gamepanel.add(btnNewButton_1);
+		
+		
+		/*-----Player vs AI-----*/
+		JLabel Image1 = new JLabel("");
+		Image1.setIcon(new ImageIcon(getClass().getResource("player.png")));
+		Image1.setBounds(23, 47, 72, 77);
+		Gamepanel.add(Image1);
+	
+		JLabel VS1 = new JLabel("");
+		VS1.setIcon(new ImageIcon(getClass().getResource("competition.png")));
+		VS1.setBounds(105, 79, 46, 45);
+		Gamepanel.add(VS1);
+		
+		JLabel Image1_1 = new JLabel("");
+		Image1_1.setIcon(new ImageIcon(getClass().getResource("ai.png")));
+		Image1_1.setBounds(161, 47, 72, 77);
+		Gamepanel.add(Image1_1);
+		
+		/*------Player vs Player -----*/
+
+		JLabel Image2 = new JLabel("");
+		Image2.setBounds(284, 47, 72, 77);
+		Image2.setIcon(new ImageIcon(getClass().getResource("player.png")));
+		Gamepanel.add(Image2);
+		
+		JLabel Image2_1 = new JLabel("");
+		Image2_1.setBounds(366, 90, 46, 14);
+		Image2_1.setIcon(new ImageIcon(getClass().getResource("competition.png")));
+		Gamepanel.add(Image2_1);
+		
+		JLabel Image2_3 = new JLabel("");
+		Image2_3.setBounds(411, 47, 64, 77);
+		Image2_3.setIcon(new ImageIcon(getClass().getResource("player.png")));
+		Gamepanel.add(Image2_3);
+		
+		JButton retour = new JButton("revient au menu");
+		retour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				setContentPane(panel);
+        		pack();
+			}
+		});
+		retour.setBounds(23, 268, 128, 23);
+		Gamepanel.add(retour);
+		return Gamepanel;
+    }
 
     /**
      * @param args the command line arguments
@@ -92,15 +198,9 @@ public class Home extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Home().setVisible(true);
-            }
-        });
+       
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton logoutBtn;
+   
     // End of variables declaration//GEN-END:variables
 }
