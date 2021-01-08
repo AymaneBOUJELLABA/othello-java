4cc7fd71ef0535a1b44644375edfdbfacdcb33f8
AymaneBOUJELLABA <aymanoosse.b.j@gmail.com>
2021-01-08 21:35:11 +0100

    GRR OTHELLOGAME DIDNT COMMIT

---
+++ src/main/java/com/othello/ai/OthelloGame.java
@@ -0,0 +1,63 @@
+package com.othello.ai;
+
+//OUR OTHELLO !!
+public class OthelloGame extends GameSearch
+{
+
+	@Override
+	public boolean drawnPosition(Position p)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean wonPosition(Position p, boolean player)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public float positionEvaluation(Position p, boolean player)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void printPosition(Position p)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Position[] possibleMoves(Position p, boolean player)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position makeMove(Position p, boolean player, Move move)
+	{
+		// TODO Auto-generated method stu
+		
+		return null;
+	}
+
+	@Override
+	public boolean reachedMaxDepth(Position p, int depth)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Move createMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
